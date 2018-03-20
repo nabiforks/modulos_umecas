206027b6d0ad0a1f077082aa5c26d01b44c82578
MiguelCabrera <miguel.cabrera.app@gmail.com>
2018-03-20 16:01:49 -0600

    [ADD] rpt_entrevista_umecas, se agrega el reporte de entrevistas de adolecentes.

---
+++ rpt_entrevista_umecas/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reporte de entrevistas / UMECAS",
+
+    'summary': """
+        Generación de reporte de entrevistas para UMECAS
+        """,
+
+    'description': """
+        Generar reporte desde la vista de evaluaciones 
+    """,
+
+    'author': "soluciones4g",
+    'website': "http://www.soluciones4g.com",
+
+    'category': 'Extra Tools',
+    'version': '1.0',
+
+    'depends': ['base', 'UMECAS'],
+
+    'data': [
+        'report/reportes_entrevista.xml',
+        'views/entrevista_inherit.xml',
+        # 'security/accesos.csv',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+}
---
+++ rpt_entrevista_umecas/models/entrevista_inherit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class Partner(models.Model):
+    _inherit = 'umc_entrevistas'
+
+    @api.multi
+    def imprimir_reporte_entrevistas(self):
+        # self.filtered(lambda s: s.state == 'draft').write({'state': 'sent'})
+        if self.state == 'terminado':
+            return self.env['report'].get_action(self, 'rpt_entrevista_umecas.detalles_entrevista')
+        else:
+            return False
