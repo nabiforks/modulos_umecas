b699d0826614281c13d98bf0f28a1ab373ec55a6
MiguelCabrera <miguel.cabrera.app@gmail.com>
2018-03-20 18:23:27 -0600

    [MDF] Cambios en el reporte de escala de riesgos de adolecentes.

--- rpt_entrevista_umecas/models/entrevista_inherit.py
+++ rpt_entrevista_umecas/models/entrevista_inherit.py
@@ -2,6 +2,7 @@
 
 from odoo import api, fields, models
 
+
 class Partner(models.Model):
     _inherit = 'umc_entrevistas'
 
@@ -10,5 +11,12 @@ class Partner(models.Model):
         # self.filtered(lambda s: s.state == 'draft').write({'state': 'sent'})
         if self.state == 'terminado':
             return self.env['report'].get_action(self, 'rpt_entrevista_umecas.detalles_entrevista')
+        else:
+            return False
+
+    @api.multi
+    def imprimir_reporte_entrevistas_escala_riesgos(self):
+        if self.state == 'terminado':
+            return self.env['report'].get_action(self, 'rpt_entrevista_umecas.detalles_entrevista_escala_riesgos')
         else:
             return False
