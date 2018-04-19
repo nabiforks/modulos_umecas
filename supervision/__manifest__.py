bd4233131e84b39980064d446d162cc9380a478b
pcs2216 <coronelsantiagop@gmail.com>
2018-04-19 17:12:46 -0500

    [IMP] supervision: Primer commit de supervision .- Estructura generada

---
+++ supervision/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Supervisión - UMECAS",
+
+    'summary': """Módulo de Supervisión para UMECAS""",
+
+    'description': """
+        Control de medidad cautelares, firmas y SCPP 
+    """,
+
+    'author': "pcs@soluciones4g",
+    'website': "http://www.soluciones4g.com",
+    'category': 'Test',
+    'version': '0.1',
+    'depends': ['base','mail','UMECAS'],
+    'data': [
+        'views/sup_menu_items.xml',
+    ],
+    'intallable': True,
+    'auto_install': False,
+}
