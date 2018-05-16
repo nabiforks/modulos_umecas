ff64938e022b9dfaee70a48451f326a5cec5d3e0
pcs2216 <coronelsantiagop@gmail.com>
2018-05-16 17:19:22 -0500

    [REF] supervision : agregar boton para confirmar eventos en visitas periódicas

--- supervision/models/sup_eventos.py
+++ supervision/models/sup_eventos.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
-
+from datetime import timedelta, datetime
 
 class sup_eventos(models.Model):
 
@@ -17,20 +17,42 @@ class sup_eventos(models.Model):
         related='x_supervision_id.x_imputado_id.display_name',
     )
     x_cumplio = fields.Boolean(
-        string=u'Cumplió/Realizado',
+        string=u'Cumplió/Realizado',        
+        readonly=True,        
+    )
+    x_atrasado = fields.Boolean(
+        string=u'Atrasado',        
+        readonly=True,        
+        compute='get_atrasados'
+    )
+    estatus = fields.Selection(
+        [('pendiente', 'Pendiente'),
+         ('realizado', 'Realizado'),
+         ('atrasado', 'Atrasado')],
+        default='pendiente',
+        readonly=True, string=u'Estatus',
+        #compute='get_atrasados'
     )
     @api.multi
-    def set_cumplio_firma(self):
-        if self.recurrency:
-            print "************************* entro pero no funciona"            
-        else:
-            print "************************* recurrente no activado"
-    
+    @api.depends('start_datetime','estatus')
+    def get_atrasados(self):
+        fecha = datetime.now()
+        for record in self:
+            fecha_inicio = fields.Datetime.from_string(record.start_datetime)
+            if fecha_inicio < fecha:
+                record.x_atrasado = True
+                record.estatus = 'atrasado'
     @api.multi
-    def action_detach_recurring_event(self):
-        
+    def set_cumplio_firma(self):
+        self.x_cumplio= True
+        self.estatus = 'realizado'
+    @api.multi
+    def set_incumplimiento(self):
+        self.x_cumplio= False
+        self.estatus = 'pendiente'
+    #@api.multi
+    def action_detach_recurring_event(self):        
         res = super(sup_eventos, self).action_detach_recurring_event()
-        self.write({'x_cumplio':True})
-        print "******************* res = ",res
-        return res
-        
+        evento = self.env['calendar.event'].search([('id', '=', res['res_id'])])
+        evento.write({'x_cumplio':True,'estatus':'realizado'})
+        return
