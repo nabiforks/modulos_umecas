715128c4318aa65278c367e38c44bb9ba091ff72
Antonio-HC <hdzant30@gmail.com>
2018-05-18 13:23:29 -0500

    [REF]supervision,reportes_supervision: sup_planeacion_actividades campos agregados

--- supervision/models/sup_planeacion_actividades.py
+++ supervision/models/sup_planeacion_actividades.py
@@ -29,9 +29,18 @@ class sup_planeacion_actividades(models.Model):
     x_dirigido_a = fields.Char(
         string='Dirigido a',
     )
+    x_cargo = fields.Text(
+        string='Cargo',
+        default='DIRECTOR DE MEDIDAS CAUTELARES Y POLICÍA PROCESAL'
+    )
+    x_observaciones = fields.Text(
+        string='Observaciones',
+    )
+    x_parrafo = fields.Text(
+        string='Presente',
+        default=lambda self:self.parrafo_default()
+    )
     
-    
-    
-    
-    
-    
+    def parrafo_default(self):
+        text = "De conformidad con los artículos 153, 154, 155, 156, 157, 164, 174, 177, 182 y 195 del Código Nacional de Procedimeintos Penales; 69 y 70 de la ley de Ejecución de Medidas Cautelares y Sanciones Penales; 17 fracciones XIII y XIV de la Ley de Seguridad Pública, todas para el Estado de Puebla, en cumplimiento a los planes de supervisión de las Medidas Cautelares y Condiciones de Suspensión Condicional, elaborados por la responsable del Área de Evaluación de Riesgos y Suspensión de Medidas Cautelares de la Región Judicial Centro, se debe realizar lo siguiente:"
+        return text
